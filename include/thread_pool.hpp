ed6b03df2edab513bd3f49904223c4a53dc744fd
caidd <986897589@qq.com>
2023-02-19 23:50:57 -0800

    threadpool finish

---
+++ include/thread_pool.hpp
@@ -0,0 +1,103 @@
+#ifndef THREAD_POOL_HPP_
+#define THREAD_POOL_HPP_
+
+#include <list>
+#include <cstdio>
+#include "locker.hpp"
+
+using std::list;
+
+template<typename Task>
+class ThreadPool {
+ public:
+  ThreadPool(int thread_num = 8, int max_request = 10000);
+  ~ThreadPool();
+  bool Append(Task *request);
+
+ private:
+  bool stop_;
+  int thread_num_;
+  int max_request_;
+
+  Sem queue_stat_;
+  Mutex queue_locker_;
+
+  pthread_t *threads_;
+  list<Task*> work_queue_;
+
+  void run();
+  static void* Worker(void *arg);
+};
+
+template<typename Task>
+ThreadPool<Task>::ThreadPool(int thread_num, int max_request)
+    : stop_(false)
+      thread_num_(thread_num),
+      max_request_(max_request) 
+      threads_(nullptr) {
+  if (thread_num_ == 0 or max_request_ <= 0) {
+    throw std::exception();
+  }
+  threads = new pthread_t[thread_num_];
+  if (!threads) {
+    throw std::exception();
+  }
+  for (int i = 0; i < thread_num_; ++ i) {
+    printf("create the %dth thread\n", i);
+    if (pthread_create(threads_ + i, NULL, Worker, this) != 0 ) {
+      delete[] threads;
+      throw std::exception();
+    }
+    if(pthread_detach(threads_[i])) {
+      delete[] threads;
+      throw std::exception();
+    }
+  }
+}
+
+template<typename Task>
+ThreadPool<Task>::~ThreadPool() {
+  delete[] threads_;
+  stop_ = true;
+}
+
+template<typename Task>
+bool ThreadPool<Task>::Append(Task *request) {
+  queue_locker_.Lock();
+  if (work_queue_.size() > max_request_) {
+    queue_locker_.Unlock();
+    return false;
+  }
+  work_queue_.emplace_back(request);
+  queue_locker_.Unlock();
+  queue_stat_.Post();
+  return true;
+}
+
+template<typename Task>
+void* ThreadPool<Task>::Worker(void *arg) {
+  auto pool = static_cast<ThreadPool*>(arg);
+  pool->run();
+  return pool;
+}
+
+template<typename Task>
+void ThreadPool<Task>::run() {
+  while (stop_) {
+    queue_stat_.Wait();
+    queue_locker_.Lock();
+    if (work_queue_.empty()) {
+      queue_locker_.Unlock();
+      continue;
+    }
+    auto request = work_queue_.front();
+    work_queue_.pop_front();
+    queue_locker_.Unlock();
+    if (!request) {
+      continue;
+    }
+    request->process();
+  }
+}
+
+#endif  // THREAD_POOL_HPP_
