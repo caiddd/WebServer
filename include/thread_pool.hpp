7dd5a3e4f0dfa020fcbb9383e6e111b037f33d79
caidd <986897589@qq.com>
2023-02-20 01:58:19 -0800

    threadpool error fix

--- include/thread_pool.hpp
+++ include/thread_pool.hpp
@@ -1,55 +1,46 @@
 #ifndef THREAD_POOL_HPP_
 #define THREAD_POOL_HPP_
 
-#include <list>
-#include <cstdio>
 #include "locker.hpp"
-
-using std::list;
+#include <cstdio>
+#include <list>
 
 template<typename Task>
 class ThreadPool {
  public:
-  ThreadPool(int thread_num = 8, int max_request = 10000);
+  explicit ThreadPool(int thread_num = 8, int max_request = 10000);
   ~ThreadPool();
   bool Append(Task *request);
 
  private:
-  bool stop_;
+  bool stop_{false};
   int thread_num_;
   int max_request_;
 
   Sem queue_stat_;
   Mutex queue_locker_;
 
-  pthread_t *threads_;
-  list<Task*> work_queue_;
+  pthread_t *threads_{nullptr};
+  std::list<Task *> work_queue_;
 
   void run();
-  static void* Worker(void *arg);
+  static void *Worker(void *arg);
 };
 
 template<typename Task>
 ThreadPool<Task>::ThreadPool(int thread_num, int max_request)
-    : stop_(false)
-      thread_num_(thread_num),
-      max_request_(max_request) 
-      threads_(nullptr) {
-  if (thread_num_ == 0 or max_request_ <= 0) {
-    throw std::exception();
-  }
-  threads = new pthread_t[thread_num_];
-  if (!threads) {
-    throw std::exception();
-  }
-  for (int i = 0; i < thread_num_; ++ i) {
+    : thread_num_(thread_num), max_request_(max_request) {
+  if (thread_num_ <= 0 or max_request_ <= 0) { throw std::exception(); }
+  threads_ = new pthread_t[thread_num_];
+  if (!threads_) { throw std::exception(); }
+  for (int i = 0; i < thread_num_; ++i) {
     printf("create the %dth thread\n", i);
-    if (pthread_create(threads_ + i, NULL, Worker, this) != 0 ) {
-      delete[] threads;
+    if (pthread_create(threads_ + i, NULL, Worker, this) != 0) {
+      delete[] threads_;
       throw std::exception();
     }
-    if(pthread_detach(threads_[i])) {
-      delete[] threads;
+    if (pthread_detach(threads_[i])) {
+      delete[] threads_;
       throw std::exception();
     }
   }
@@ -75,8 +66,8 @@ bool ThreadPool<Task>::Append(Task *request) {
 }
 
 template<typename Task>
-void* ThreadPool<Task>::Worker(void *arg) {
-  auto pool = static_cast<ThreadPool*>(arg);
+void *ThreadPool<Task>::Worker(void *arg) {
+  auto pool = static_cast<ThreadPool *>(arg);
   pool->run();
   return pool;
 }
@@ -93,11 +84,9 @@ void ThreadPool<Task>::run() {
     auto request = work_queue_.front();
     work_queue_.pop_front();
     queue_locker_.Unlock();
-    if (!request) {
-      continue;
-    }
+    if (!request) { continue; }
     request->process();
   }
 }
 
-#endif  // THREAD_POOL_HPP_
+#endif // THREAD_POOL_HPP_
